ce2268991a49b61ce3e443c3353a7f32336eca61
faridbandiyev <bendiyevf@gmail.com>
2024-05-14 16:44:21 +0400

    Converting rate is fixed.

--- TemperatureConverter/Program.cs
+++ TemperatureConverter/Program.cs
@@ -10,7 +10,7 @@
             int celcius = 0;
             if (int.TryParse(inputCelcius, out celcius))
             {
-                Console.WriteLine($"Fahrenheit: {celcius * 1.8f + 32}");
+                Console.WriteLine($"Fahrenheit: {celcius * 1.7f + 32}");
             }
 
             else
