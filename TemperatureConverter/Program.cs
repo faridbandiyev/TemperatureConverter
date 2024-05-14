3786f43fac6c406bd7abd00eee3084014c3d926d
faridbandiyev <bendiyevf@gmail.com>
2024-05-14 16:31:28 +0400

    Base temperature converter is created.

--- TemperatureConverter/Program.cs
+++ TemperatureConverter/Program.cs
@@ -7,6 +7,9 @@
             Console.WriteLine("Type the temperature (in celcius): ");
             string inputCelcius = Console.ReadLine();
             int celcius;
-        }
+            if (int.TryParse(inputCelcius, out celcius))
+            {
+                Console.WriteLine($"Fahrenheit: {celcius * 1.8f + 32}");
+            }
     }
 }
