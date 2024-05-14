dbf040393f3ee9c1e9d756efd3f32e3fb839efbe
faridbandiyev <bendiyevf@gmail.com>
2024-05-14 16:30:48 +0400

    User input functionality is added.

--- TemperatureConverter/Program.cs
+++ TemperatureConverter/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Type the temperature (in celcius): ");
+            string inputCelcius = Console.ReadLine();
+            int celcius;
         }
     }
 }
