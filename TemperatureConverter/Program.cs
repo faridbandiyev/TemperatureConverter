4f48db35e6ff31fd5d6eea2edce3e12011a05fcd
faridbandiyev <bendiyevf@gmail.com>
2024-05-14 16:30:04 +0400

    Add project files.

---
+++ TemperatureConverter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TemperatureConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type the temperature (in celcius): ");
+        }
+    }
+}
